99f43b95829be86f1073dbd7d936c160bd202576
jianghao233 <jianghao18395297677@gmail.com>
2017-11-13 21:25:11 +0800

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by acer on 2017/11/4.
+ */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+       items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+    });
+});
